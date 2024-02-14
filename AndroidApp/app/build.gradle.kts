edbce41785f138a90e0d5bc36a81554aa4b9459f
Grigory Entin <grigory.entin@gmail.com>
2024-02-15 00:31:36 +0100

    Imported AndroidApp from Comm Android of https://github.com/garmin/connectiq-android-sdk@ca7bcf8.

---
+++ AndroidApp/app/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+val compileSdkVersion: String by project
+val minSdkVersion: String by project
+val targetSdkVersion: String by project
+val packageName = "com.garmin.android.apps.connectiq.sample.comm"
+val versionCode: String by project
+val versionName: String by project
+
+android {
+    namespace = this@Build_gradle.packageName
+    compileSdk = this@Build_gradle.compileSdkVersion.toInt()
+
+    defaultConfig {
+        applicationId = this@Build_gradle.packageName
+        minSdk = minSdkVersion.toInt()
+        targetSdk = targetSdkVersion.toInt()
+        versionCode = this@Build_gradle.versionCode.toInt()
+        versionName = this@Build_gradle.versionName
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation("com.garmin.connectiq:ciq-companion-app-sdk:2.0.3@aar")
+}
---
+++ AndroidApp/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "7.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
