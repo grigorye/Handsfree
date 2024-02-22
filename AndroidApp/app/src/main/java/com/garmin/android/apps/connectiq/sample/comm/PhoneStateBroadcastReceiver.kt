0df5a5956daceddd6347a20c552536c8ec11832f
Grigory Entin <grigory.entin@gmail.com>
2024-02-22 14:37:34 +0100

    Added intent extras based call monitoring.

--- AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/PhoneStateBroadcastReceiver.kt
+++ AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/PhoneStateBroadcastReceiver.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
+import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.LifecycleCoroutineScope
 import com.garmin.android.apps.connectiq.sample.comm.activities.globalConnectIQ
@@ -20,24 +21,68 @@ import kotlinx.coroutines.withContext
 
 
 class PhoneStateBroadcastReceiver : BroadcastReceiver() {
+    companion object {
+        private var listener: CustomPhoneStateListener? = null
+        private val TAG: String = PhoneStateBroadcastReceiver::class.java.simpleName
+    }
+
     override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Intent: ${intent}")
+        val stateExtra = intent.getStringExtra(TelephonyManager.EXTRA_STATE)!!
+        val incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+        Log.d(TAG, "stateExtra: ${stateExtra}")
+        Log.d(TAG, "incomingNumber: ${incomingNumber}")
+
+        var lifecycleCoroutineScope: LifecycleCoroutineScope = globalLifecycleCoroutineScope!!
+        var connectIQ: ConnectIQ = globalConnectIQ!!
+        var device: IQDevice = globalDevice!!
+
+        sendCallStateExtra(stateExtra, incomingNumber ?: "") { message, listener ->
+            lifecycleCoroutineScope.launch {
+                withContext(Dispatchers.IO) {
+                    connectIQ.sendMessage(device, myApp, message, listener)
+                }
+            }
+        }
+
+        return;
+
+        listener = CustomPhoneStateListener(context, globalLifecycleCoroutineScope!!, globalConnectIQ!!, globalDevice!!, myApp)
         val telephonyManager = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
         telephonyManager.listen(
-            CustomPhoneStateListener(context, globalLifecycleCoroutineScope!!, globalConnectIQ!!, globalDevice!!, myApp),
+            listener,
             PhoneStateListener.LISTEN_CALL_STATE
         )
     }
 }
+
+var lastIncomingNumber: String = ""
+var lastState: Int = -1
 
 class CustomPhoneStateListener(//private static final String TAG = "PhoneStateChanged";
     var context: Context, //Context to make Toast if required
     var lifecycleCoroutineScope: LifecycleCoroutineScope,
     var connectIQ: ConnectIQ,
     var device: IQDevice,
-    var app: IQApp
+    var app: IQApp,
 ) : PhoneStateListener() {
+    companion object {
+        private val TAG: String = CustomPhoneStateListener::class.java.simpleName
+    }
     override fun onCallStateChanged(state: Int, incomingNumber: String) {
         super.onCallStateChanged(state, incomingNumber)
+        val incomingNumberDidNotChange = incomingNumber == lastIncomingNumber
+        val incomingNumberIsDummy = incomingNumber == ""
+        Log.d(TAG, "state(old) = ${state}(${lastState}), incomingNumber(old)=${incomingNumber}(${lastIncomingNumber})")
+        Log.d(TAG, "incomingNumberDidNotChange: ${incomingNumberDidNotChange}")
+        Log.d(TAG, "incomingNumberIsDummy: ${incomingNumberIsDummy}")
+        if (lastState == state && (incomingNumberDidNotChange || incomingNumberIsDummy)) {
+            Log.d(TAG, "Ignoring lastState: ${lastState}, incomingNumber: ${lastIncomingNumber}")
+            return;
+        }
+        lastState = state;
+        lastIncomingNumber = incomingNumber;
+        Log.d(TAG, "Sending state: ${state}, ")
         sendCallState(state, incomingNumber) { message, listener ->
             lifecycleCoroutineScope.launch {
                 withContext(Dispatchers.IO) {
@@ -45,6 +90,40 @@ class CustomPhoneStateListener(//private static final String TAG = "PhoneStateCh
                 }
             }
         }
+    }
+}
+
+fun sendCallStateExtra(extra: String, incomingNumber: String, send: (message: Any, listener: ConnectIQ.IQSendMessageListener) -> Unit) {
+    when (extra) {
+        TelephonyManager.EXTRA_STATE_IDLE -> {
+            val msg = mapOf(
+                "cmd" to "noCallInProgress"
+            )
+            send(msg) { _, _, status ->
+                print(status)
+            }
+        }
+
+        TelephonyManager.EXTRA_STATE_OFFHOOK -> {
+            val msg = mapOf(
+                "cmd" to "callInProgress",
+                "number" to incomingNumber
+            )
+            send(msg) { _, _, status ->
+                print(status)
+            }
+        }
+
+        TelephonyManager.EXTRA_STATE_RINGING -> {
+            val msg = mapOf(
+                "cmd" to "ringing"
+            )
+            send(msg) { _, _, status ->
+                print(status)
+            }
+        }
+
+        else -> {}
     }
 }
 
