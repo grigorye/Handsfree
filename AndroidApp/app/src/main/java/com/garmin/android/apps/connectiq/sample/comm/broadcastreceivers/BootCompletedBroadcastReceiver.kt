dcccb7a34a4ad5efc0a5d53bd9d7e295e3354f62
Grigory Entin <grigory.entin@gmail.com>
2024-02-27 02:18:36 +0100

    Made foreground service started on startup.

--- AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/broadcastreceivers/BootCompletedBroadcastReceiver.kt
+++ AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/broadcastreceivers/BootCompletedBroadcastReceiver.kt
@@ -4,14 +4,17 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.content.ContextCompat.startForegroundService
+import com.garmin.android.apps.connectiq.sample.comm.services.GarminPhoneCallConnectorService
 
 class BootCompletedBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d(TAG, "Intent: ${intent}")
+        Log.d(TAG, "Intent: $intent")
+        startForegroundService(context, Intent(context, GarminPhoneCallConnectorService::class.java))
     }
 
     companion object {
-        private val TAG: String = PhoneStateBroadcastReceiver::class.java.simpleName
+        private val TAG: String = BootCompletedBroadcastReceiver::class.java.simpleName
     }
 }
