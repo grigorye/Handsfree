c40686fec09ded9483e4dddb2a47eda48b74f388
Grigory Entin <grigory.entin@gmail.com>
2024-02-28 23:43:22 +0100

    Added ConnectIQIncomingMessageBroadcastReceiver.

---
+++ AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/broadcastreceivers/ConnectIQIncomingMessageBroadcastReceiver.kt
@@ -0,0 +1,24 @@
+package com.garmin.android.apps.connectiq.sample.comm.broadcastreceivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import com.garmin.android.apps.connectiq.sample.comm.services.GarminPhoneCallConnectorService
+
+class ConnectIQIncomingMessageBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        Log.d(TAG, "Intent: $intent")
+        val intentForConnector =
+            Intent(context, GarminPhoneCallConnectorService::class.java).apply {
+                action = intent.action
+                putExtras(intent)
+            }
+        context.startForegroundService(intentForConnector);
+    }
+
+    companion object {
+        val TAG: String = ConnectIQIncomingMessageBroadcastReceiver::class.java.simpleName
+    }
+}
