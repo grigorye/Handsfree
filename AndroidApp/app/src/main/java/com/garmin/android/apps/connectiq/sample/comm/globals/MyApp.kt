66654ccbcfb5816580a3e09ec77300620067c390
Grigory Entin <grigory.entin@gmail.com>
2024-02-28 01:04:48 +0100

    Reformatted.

--- AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/globals/MyApp.kt
+++ AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/globals/MyApp.kt
@@ -3,5 +3,9 @@ package com.garmin.android.apps.connectiq.sample.comm.globals
 import com.garmin.android.apps.connectiq.sample.comm.impl.isRunningInEmulator
 import com.garmin.android.connectiq.IQApp
 
-val COMM_WATCH_ID = if (isRunningInEmulator()) { "" } else { "a3421feed289106a538cb9547ab12095" }
+val COMM_WATCH_ID = if (isRunningInEmulator()) {
+    ""
+} else {
+    "a3421feed289106a538cb9547ab12095"
+}
 val myApp = IQApp(COMM_WATCH_ID)
