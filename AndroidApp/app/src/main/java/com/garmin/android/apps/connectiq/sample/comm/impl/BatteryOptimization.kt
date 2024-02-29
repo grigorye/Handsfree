47a729af5c42ea74e24436af966f99b98b8d0633
Grigory Entin <grigory.entin@gmail.com>
2024-02-29 01:15:40 +0100

    Dropped redundant check for Build.VERSION_CODES.M.

--- AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/impl/BatteryOptimization.kt
+++ AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/impl/BatteryOptimization.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Context.POWER_SERVICE
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.PowerManager
 import android.provider.Settings
 import android.util.Log
@@ -12,16 +11,14 @@ import android.util.Log
 fun requestIgnoreBatteryOptimizations(context: Context) {
     val tag = object {}.javaClass.enclosingMethod?.name
 
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        val packageName = context.packageName
-        val pm = context.getSystemService(POWER_SERVICE) as PowerManager
-        val isIgnoringBatteryOptimizations = pm.isIgnoringBatteryOptimizations(packageName)
-        Log.i(tag, "isIgnoringBatteryOptimizations: $isIgnoringBatteryOptimizations")
-        if (!isIgnoringBatteryOptimizations) {
-            val intent = Intent()
-            intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
-            intent.setData(Uri.parse("package:$packageName"))
-            context.startActivity(intent)
-        }
+    val packageName = context.packageName
+    val pm = context.getSystemService(POWER_SERVICE) as PowerManager
+    val isIgnoringBatteryOptimizations = pm.isIgnoringBatteryOptimizations(packageName)
+    Log.i(tag, "isIgnoringBatteryOptimizations: $isIgnoringBatteryOptimizations")
+    if (!isIgnoringBatteryOptimizations) {
+        val intent = Intent()
+        intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+        intent.setData(Uri.parse("package:$packageName"))
+        context.startActivity(intent)
     }
 }
