5a5487587150042ffcb173a5d1d48a2404196eab
Grigory Entin <grigory.entin@gmail.com>
2024-03-02 22:09:27 +0100

    Revert "Disabled starting the connector on boot, due to problems with initialization of SDK at that moment."
    This reverts commit 2b55763975328e9119cf3cc7b31168cd5ac3fc50.

--- AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/broadcastreceivers/BootCompletedBroadcastReceiver.kt
+++ AndroidApp/app/src/main/java/com/garmin/android/apps/connectiq/sample/comm/broadcastreceivers/BootCompletedBroadcastReceiver.kt
@@ -12,14 +12,7 @@ class BootCompletedBroadcastReceiver : BroadcastReceiver() {
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
     override fun onReceive(context: Context, intent: Intent) {
         Log.d(TAG, "intent: $intent")
-        if (!keepAwakeEnabled) {
-            // Attempts to launch it on boot fail because the service running inside GarminConnect
-            // dies. On the other hand, it's not necessary to launch it on boot *unless* we rely
-            // on e.g. alarms to relaunch the app (and we don't: relying on INCOMING_MESSAGE as
-            // a trigger instead).
-        } else {
-            startConnector(context, intent)
-        }
+        startConnector(context, intent)
     }
 
     companion object {
