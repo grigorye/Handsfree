64f7390e22380849198c0f53454f999590359eec
Grigory Entin <grigory.entin@gmail.com>
2024-03-07 02:16:54 +0100

    Tweaked to suppress deprecation on import.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/contacts/OpeningContacts.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/contacts/OpeningContacts.kt
@@ -2,11 +2,10 @@ package com.gentin.connectiq.handsfree.contacts
 
 import android.content.Context
 import android.content.Intent
-import android.provider.Contacts
 
 
 fun openFavorites(context: Context) {
     @Suppress("DEPRECATION")
-    val intent = Intent(Contacts.Intents.UI.LIST_STARRED_ACTION)
+    val intent = Intent(android.provider.Contacts.Intents.UI.LIST_STARRED_ACTION)
     context.startActivity(intent)
 }
