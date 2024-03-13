2751c97e1232b736f8c68b7d273177b0dc95bd6d
Grigory Entin <grigory.entin@gmail.com>
2024-03-14 00:09:44 +0100

    Tweaked the permission list checked for different SDK versions; added more logging.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/permissions/ManifestPermissions.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/permissions/ManifestPermissions.kt
@@ -2,6 +2,7 @@ package com.gentin.connectiq.handsfree.permissions
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.os.Build
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
@@ -10,29 +11,45 @@ val manifestPermissions = arrayOf(
     Manifest.permission.ANSWER_PHONE_CALLS,
     Manifest.permission.CALL_PHONE,
     Manifest.permission.FOREGROUND_SERVICE,
-    Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE,
-    Manifest.permission.POST_NOTIFICATIONS,
     Manifest.permission.READ_CALL_LOG,
     Manifest.permission.READ_CONTACTS,
     Manifest.permission.READ_PHONE_STATE,
     Manifest.permission.RECEIVE_BOOT_COMPLETED,
     Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
-    Manifest.permission.SYSTEM_ALERT_WINDOW
-)
+    // SYSTEM_ALERT_WINDOW is checked via Settings.canDrawOverlays()/OverlayPermissionsHandler
+    // Manifest.permission.SYSTEM_ALERT_WINDOW,
+) + manifestPermissionsExtrasUpsideDownCake() + manifestPermissionsExtrasTiramisu()
+
+fun manifestPermissionsExtrasUpsideDownCake(): Array<String> {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+        arrayOf(
+            Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE,
+        )
+    } else {
+        arrayOf()
+    }
+}
+
+fun manifestPermissionsExtrasTiramisu(): Array<String> {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        arrayOf(
+            Manifest.permission.POST_NOTIFICATIONS,
+        )
+    } else {
+        arrayOf()
+    }
+}
 
 val manifestPermissionsHandler = PermissionsHandler(
     hasPermission = { context ->
-        manifestPermissions.all {
+        manifestPermissions.all { permission ->
             val tag = object {}.javaClass.enclosingMethod?.name
-            if (ContextCompat.checkSelfPermission(
+            val hasPermission = ContextCompat.checkSelfPermission(
                 context,
-                it
-            ) != PackageManager.PERMISSION_GRANTED) {
-                Log.d(tag, "Missing permission: $it")
-                false
-            } else {
-                true
-            }
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+            Log.d(tag, "$permission: $hasPermission")
+            hasPermission
         }
     },
     requestPermission = { context ->
