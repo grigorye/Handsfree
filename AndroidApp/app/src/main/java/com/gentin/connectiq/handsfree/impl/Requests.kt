29e3d6a7206cd8c7a3e4068f70b0f9a22919acf6
Grigory Entin <grigory.entin@gmail.com>
2024-05-27 20:38:06 +0200

    Dropped unused Command.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/impl/Requests.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/impl/Requests.kt
@@ -3,12 +3,6 @@ package com.gentin.connectiq.handsfree.impl
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class Command(val key: String) {
-    CALL("call"),
-    HANGUP("hangup")
-}
-
-@Serializable
 data class CommonRequest(
     val cmd: String
 )
