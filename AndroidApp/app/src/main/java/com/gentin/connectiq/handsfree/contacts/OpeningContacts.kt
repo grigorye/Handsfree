1e9e1d4d5a3f894e3aa2a490dc5b7b93c77ffd58
Grigory Entin <grigory.entin@gmail.com>
2024-06-05 20:47:33 +0200

    Added workaround for (deprecated) android.provider.Contacts.Intents.UI.LIST_STARRED_ACTION throwing an exception.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/contacts/OpeningContacts.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/contacts/OpeningContacts.kt
@@ -2,10 +2,23 @@ package com.gentin.connectiq.handsfree.contacts
 
 import android.content.Context
 import android.content.Intent
+import android.provider.ContactsContract
+import android.util.Log
 
 
 fun openFavorites(context: Context) {
-    @Suppress("DEPRECATION")
-    val intent = Intent(android.provider.Contacts.Intents.UI.LIST_STARRED_ACTION)
-    context.startActivity(intent)
+    val tag = object {}.javaClass.enclosingMethod?.name
+
+    try {
+        @Suppress("DEPRECATION")
+        val intent = Intent(android.provider.Contacts.Intents.UI.LIST_STARRED_ACTION)
+        context.startActivity(intent)
+    } catch (e: RuntimeException) {
+        Log.d(tag, "OldStyleOpenFavoritesFailed: $e")
+
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = ContactsContract.Contacts.CONTENT_URI
+        }
+        context.startActivity(intent)
+    }
 }
