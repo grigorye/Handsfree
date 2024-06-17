0fd6b29066ebd072531d4b5a6bd5f62d0ce1c95b
Grigory Entin <grigory.entin@gmail.com>
2024-06-18 01:20:04 +0200

    Upgraded kotlin to 2.0.0.

--- AndroidApp/build.gradle.kts
+++ AndroidApp/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     kotlin("plugin.serialization") version "1.9.22"
 }
 
