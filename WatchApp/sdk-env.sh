a77310c88466acd75428d038039be37624f3bf3b
Grigory Entin <grigory.entin@gmail.com>
2024-06-23 05:47:18 +0200

    Simplified sdk_root computation.

--- WatchApp/sdk-env.sh
+++ WatchApp/sdk-env.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 
-latest_sdk_version="7.1.1-2024-04-11-66d0159ae"
-stable_sdk_version="7.1.1-2024-04-11-66d0159ae"
+sdk_version="7.1.1-2024-04-11-66d0159ae"
 
 developer_key="$src_root/../../Handsfree-Publishing/keys/developer_key"
 
 sdks_dir=~/Library/Application\ Support/Garmin/ConnectIQ/Sdks
-latest_sdk_root="$sdks_dir/connectiq-sdk-mac-$latest_sdk_version"
-stable_sdk_root="$sdks_dir/connectiq-sdk-mac-$stable_sdk_version"
+sdk_root="$sdks_dir/connectiq-sdk-mac-$sdk_version"
 
-export sdk_root="$latest_sdk_root"
+export sdk_root
