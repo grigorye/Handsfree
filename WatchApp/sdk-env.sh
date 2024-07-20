857f8a88d252fe81c18242bbd750cd8bdd154af0
Grigory Entin <grigory.entin@gmail.com>
2024-07-21 01:49:38 +0200

    Added support for CIQ_DEV_KEY/CIQ_SDK_ROOT.

--- WatchApp/sdk-env.sh
+++ WatchApp/sdk-env.sh
@@ -2,9 +2,9 @@
 
 sdk_version=$(cat "$src_root/sdk-version.txt")
 
-developer_key="$src_root/../../Handsfree-Publishing/keys/developer_key"
+developer_key="${CIQ_DEV_KEY:-$src_root/../../Handsfree-Publishing/keys/developer_key}"
 
 sdks_dir=~/Library/Application\ Support/Garmin/ConnectIQ/Sdks
-sdk_root="$sdks_dir/connectiq-sdk-mac-$sdk_version"
+sdk_root="${CIQ_SDK_ROOT:-$sdks_dir/connectiq-sdk-mac-$sdk_version}"
 
 export sdk_root
