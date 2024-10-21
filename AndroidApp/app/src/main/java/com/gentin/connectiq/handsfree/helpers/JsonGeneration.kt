d6aa9af6d3724166a4c508aa4d342a7ce5c76445
Grigory Entin <grigory.entin@gmail.com>
2024-10-22 00:10:59 +0200

    Employed kotlinx for pojo generation (to account @SerialName and etc.).

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/helpers/JsonGeneration.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/helpers/JsonGeneration.kt
@@ -1,12 +1,15 @@
 package com.gentin.connectiq.handsfree.helpers
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
 
 
 val mapper = jacksonObjectMapper()
 
 inline fun <reified T> pojoMap(o: T): Any {
-    return mapper.convertValue(o, Map::class.java)
+    val string = Json.encodeToString(o)
+    return mapper.readValue(string, Map::class.java)
 }
 
 inline fun <reified T> pojoList(o: T): Any {
