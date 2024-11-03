d300104676194601a1872a76005a18084a3e79ea
Grigory Entin <grigory.entin@gmail.com>
2024-11-03 12:17:13 +0100

    Sorted cmd definitions/segregated v1 group.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/terms/Cmd.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/terms/Cmd.kt
@@ -1,10 +1,11 @@
 package com.gentin.connectiq.handsfree.terms
 
-const val syncYouCmd = "syncYou"
-const val acceptQueryResultCmd = "a"
-const val setPhonesV1Cmd = "setPhones"
 const val phoneStateChangedV1Cmd = "phoneStateChanged"
-const val phoneStateChangedCmd = "p"
+const val setPhonesV1Cmd = "setPhones"
+const val syncYouCmd = "syncYou"
+
+const val acceptQueryResultCmd = "a"
 const val openAppFailedCmd = "f"
 const val openMeCompletedCmd = "c"
+const val phoneStateChangedCmd = "p"
 const val subjectsChangedCmd = "s"
