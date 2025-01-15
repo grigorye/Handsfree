f3cd7b0222a0fb49cb8fd38a029bd81324e8cbd5
Grigory Entin <grigory.entin@gmail.com>
2025-01-15 03:23:02 +0100

    Added support for invalidating subjects.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/activities/DelegatedToService.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/activities/DelegatedToService.kt
@@ -7,3 +7,7 @@ import com.gentin.connectiq.handsfree.impl.startConnector
 fun invalidatePermissions(context: Context, permissions: Array<String>, grantResults: IntArray) {
     startConnector(context, ACTIVATE_AND_RECONNECT)
 }
+
+fun invalidateSubjects(context: Context) {
+    startConnector(context, ACTIVATE_AND_RECONNECT)
+}
