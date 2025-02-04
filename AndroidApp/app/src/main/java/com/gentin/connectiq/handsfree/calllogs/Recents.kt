8984ee9e8f8de7181b07ed0300296c774ab5acdb
Grigory Entin <grigory.entin@gmail.com>
2025-02-05 00:15:50 +0100

    Dropped limiting the result in recentsFromCallLog in favor of applying the limitation in recentsPojo.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/calllogs/Recents.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/calllogs/Recents.kt
@@ -1,7 +1,5 @@
 package com.gentin.connectiq.handsfree.calllogs
 
-private const val maxRecentsCount = 5
-
 fun recentsFromCallLog(callLog: List<CallLogEntry>): List<CallLogEntry> {
     return callLog.fold(listOf<CallLogEntry>()) { acc, callLogEntry ->
         if (acc.find { x -> x.number == callLogEntry.number } != null) {
@@ -9,5 +7,5 @@ fun recentsFromCallLog(callLog: List<CallLogEntry>): List<CallLogEntry> {
         } else {
             acc + callLogEntry
         }
-    }.take(maxRecentsCount)
+    }
 }
