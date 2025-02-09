f4559389815e74ac6f8d543c5e3c5103b8d40c9b
Grigory Entin <grigory.entin@gmail.com>
2025-02-09 23:14:28 +0100

    Tweaked for running in emulator on Intel.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/helpers/IsRunningInEmulator.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/helpers/IsRunningInEmulator.kt
@@ -3,5 +3,8 @@ package com.gentin.connectiq.handsfree.helpers
 import android.os.Build
 
 fun isRunningInEmulator(): Boolean {
-    return Build.DEVICE == "emu64a"
+    if (Build.DEVICE == "emu64a" || Build.DEVICE == "emu64xa") {
+        return true
+    }
+    return false
 }
