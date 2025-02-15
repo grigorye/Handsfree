daad577d5af79710f00bf052fc0bbe759f710a9a
Grigory Entin <grigory.entin@gmail.com>
2025-02-15 15:41:50 +0100

    Dropped redundant typing.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/calllogs/Recents.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/calllogs/Recents.kt
@@ -1,7 +1,7 @@
 package com.gentin.connectiq.handsfree.calllogs
 
 fun recentsFromCallLog(callLog: List<CallLogEntry>): List<CallLogEntry> {
-    return callLog.fold(listOf<CallLogEntry>()) { acc, callLogEntry ->
+    return callLog.fold(listOf()) { acc, callLogEntry ->
         if (acc.find { x -> x.number == callLogEntry.number } != null) {
             acc
         } else {
