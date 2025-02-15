22f32d5b1ca89b3a97d1a01b12af42e6160b4cc4
Grigory Entin <grigory.entin@gmail.com>
2025-02-15 15:44:35 +0100

    Upgraded agp.

--- AndroidApp/build.gradle.kts
+++ AndroidApp/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.8.0" apply false
+    id("com.android.application") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
     kotlin("plugin.serialization") version "1.9.22"
 }
