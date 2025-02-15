ac6db0257d1e0830b8f6e0a2cc7f8630189a6ae1
Grigory Entin <grigory.entin@gmail.com>
2025-02-15 15:42:51 +0100

    Simplified expressions.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/helpers/IsRunningInEmulator.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/helpers/IsRunningInEmulator.kt
@@ -3,8 +3,5 @@ package com.gentin.connectiq.handsfree.helpers
 import android.os.Build
 
 fun isRunningInEmulator(): Boolean {
-    if (Build.DEVICE == "emu64a" || Build.DEVICE == "emu64xa") {
-        return true
-    }
-    return false
+    return Build.DEVICE == "emu64a" || Build.DEVICE == "emu64xa"
 }
