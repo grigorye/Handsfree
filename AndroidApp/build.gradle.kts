98397e983eead5a0f79e03d5a04df9764e69d709
Grigory Entin <grigory.entin@gmail.com>
2025-06-05 18:57:44 +0200

    Upgraded android plugin.

--- AndroidApp/build.gradle.kts
+++ AndroidApp/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application") version "8.10.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     kotlin("plugin.serialization") version "1.9.22"
 }
 
