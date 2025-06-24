632a19b81e353977921e853867d17a3405eb58be
Grigory Entin <grigory.entin@gmail.com>
2025-06-24 21:02:32 +0200

    Upgraded dependencies.

--- AndroidApp/build.gradle.kts
+++ AndroidApp/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application") version "8.11.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    kotlin("plugin.serialization") version "1.9.22"
+    id("org.jetbrains.kotlin.android") version "2.2.0" apply false
+    kotlin("plugin.serialization") version "2.2.0"
 }
 
 tasks.register("clean", Delete::class) {
