312adb25f9cdebab709e11cb1b32eee000bb7001
Grigory Entin <grigory.entin@gmail.com>
2025-06-24 20:00:43 +0200

    Introduced faster, visitor-based recents.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/calllogs/Recents.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/calllogs/Recents.kt
@@ -1,12 +1,25 @@
 package com.gentin.connectiq.handsfree.calllogs
 
-fun recentsFromCallLog(callLogsRepository: CallLogsRepository): List<CallLogEntry> {
+fun recentsFromCallLog(callLogsRepository: CallLogsRepository, limit: Int): List<CallLogEntry> {
+    val acc: MutableList<CallLogEntry> = mutableListOf()
+    callLogsRepository.accept { callLogEntry ->
+        if (acc.find { x -> x.number == callLogEntry.number } != null) {
+            return@accept true
+        }
+        acc += callLogEntry
+        return@accept acc.size <= limit
+    }
+    return acc
+}
+
+fun recentsFromCallLogV1(callLogsRepository: CallLogsRepository, limit: Int): List<CallLogEntry> {
     val callLog = callLogsRepository.callLog()
-    return callLog.fold(listOf()) { acc, callLogEntry ->
+    val recents: List<CallLogEntry> = callLog.fold(listOf()) { acc, callLogEntry ->
         if (acc.find { x -> x.number == callLogEntry.number } != null) {
             acc
         } else {
             acc + callLogEntry
         }
     }
+    return recents.take(limit)
 }
