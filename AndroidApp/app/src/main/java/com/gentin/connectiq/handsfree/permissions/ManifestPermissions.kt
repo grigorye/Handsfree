bd7aad232a9b85236b42226da635f4ba3f35fcda
Grigory Entin <grigory.entin@gmail.com>
2025-07-05 23:49:11 +0200

    Dropped no longer used stuff.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/permissions/ManifestPermissions.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/permissions/ManifestPermissions.kt
@@ -1,69 +1,14 @@
 package com.gentin.connectiq.handsfree.permissions
 
-import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
-import android.os.Build
 import android.util.Log
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
-private val baseManifestPermissions = listOf(
-    Manifest.permission.ANSWER_PHONE_CALLS,
-    Manifest.permission.CALL_PHONE,
-    Manifest.permission.FOREGROUND_SERVICE,
-    Manifest.permission.READ_CONTACTS,
-    Manifest.permission.READ_PHONE_STATE,
-    Manifest.permission.RECEIVE_BOOT_COMPLETED,
-    Manifest.permission.REQUEST_IGNORE_BATTERY_OPTIMIZATIONS,
-    // SYSTEM_ALERT_WINDOW is checked via Settings.canDrawOverlays()/OverlayPermissionHandler
-    // Manifest.permission.SYSTEM_ALERT_WINDOW,
-)
-
-private fun manifestPermissions(context: Context): List<String> {
-    return baseManifestPermissions + manifestPermissionsRejectedByGooglePlay(context) + manifestPermissionsExtrasUpsideDownCake() + manifestPermissionsExtrasTiramisu()
-}
-
 fun isPermissionRequested(context: Context, permission: String): Boolean {
     val info = context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_PERMISSIONS)
     return info.requestedPermissions?.contains(permission) == true
-}
-
-fun manifestPermissionsRejectedByGooglePlay(context: Context): List<String> {
-    return if (isPermissionRequested(context, Manifest.permission.READ_CALL_LOG)) {
-        listOf(
-            Manifest.permission.READ_CALL_LOG,
-        )
-    } else {
-        listOf()
-    }
-}
-
-fun manifestPermissionsExtrasUpsideDownCake(): List<String> {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
-        listOf(
-            Manifest.permission.FOREGROUND_SERVICE_SPECIAL_USE,
-        )
-    } else {
-        listOf()
-    }
-}
-
-fun manifestPermissionsExtrasTiramisu(): List<String> {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-        listOf(
-            Manifest.permission.POST_NOTIFICATIONS,
-        )
-    } else {
-        listOf()
-    }
-}
-
-fun newManifestPermissionHandler(context: Context): PermissionHandler {
-    return newManifestPermissionHandler(
-        requiredPermissions = manifestPermissions(context),
-        optionalPermissions = listOf()
-    )
 }
 
 fun hasPermissions(context: Context, permissions: List<String>): Boolean {
--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/permissions/Permissions.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/permissions/Permissions.kt
@@ -1,30 +1,9 @@
 package com.gentin.connectiq.handsfree.permissions
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
-
-fun registeredPermissionHandlers(context: Context): Array<PermissionHandler> {
-    return arrayOf(
-        newManifestPermissionHandler(context),
-        batteryOptimizationPermissionHandler,
-        overlayPermissionHandler
-    )
-}
-
-fun anyPermissionMissing(context: Activity): Boolean {
-    return registeredPermissionHandlers(context).any { !it.hasPermission(context) }
-}
-
-fun requestPermissions(context: Activity) {
-    registeredPermissionHandlers(context).forEach {
-        if (!it.hasPermission(context)) {
-            it.requestPermission(context)
-        }
-    }
-}
 
 fun openAppSettings(context: Context) {
     openSettings(context, context.packageName)
