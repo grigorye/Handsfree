ac3c879cad39ff3ffadbcef6cc67a1d50352a5eb
Grigory Entin <grigory.entin@gmail.com>
2025-07-15 18:01:45 +0200

    Added support for opening prod widget in Connect IQ.

--- AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/globals/MyApp.kt
+++ AndroidApp/app/src/main/java/com/gentin/connectiq/handsfree/globals/MyApp.kt
@@ -13,6 +13,7 @@ private val betaWidget = IQApp("cdc225d9-bc88-4d9e-a8a6-ce7dffdd0714", "Handsfre
 fun storeID(app: IQApp): String? {
     return when (app) {
         prodApp -> "73107243-f322-4cf2-bb3d-78f2a4ee8920"
+        prodWidget -> "629f3ba5-66bc-4b44-a078-3365a85212f8"
         betaApp -> "debb75b5-1c5e-4bd3-bff3-3a2a8b3e7fbf"
         betaWidget -> "a9a847b7-d15f-493d-8242-efadbdd9cdf2"
         else -> null
